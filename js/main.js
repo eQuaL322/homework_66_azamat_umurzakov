c022d25a7245e92d8f8b8fd96c6ec1e5e8aa5c45
Azamat Umurzakov <equal32296@gmail.com>
2023-04-03 10:10:54 +0600

    Сделал второе задание

--- js/main.js
+++ js/main.js
@@ -11,3 +11,12 @@ console.log(str2)
 console.log(str6)
 console.log(str4)
 console.log(str1)
+
+
+const elements = document.getElementsByClassName('element')
+elements[0].style.color = 'red'
+elements[1].style.color = 'red'
+elements[2].style.color = 'red'
+elements[3].style.color = 'green'
+elements[4].style.color = 'green'
+elements[5].style.color = 'green'
