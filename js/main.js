50a40754abb54f52c9662a25d4f9b7e6a7051781
Azamat Umurzakov <equal32296@gmail.com>
2023-04-03 10:06:20 +0600

    Сделал первое задание

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const str1 = document.getElementById('string-1')
+const str2 = document.getElementById('string-2')
+const str3 = document.getElementById('string-3')
+const str4 = document.getElementById('string-4')
+const str5 = document.getElementById('string-5')
+const str6 = document.getElementById('string-6')
+
+console.log(str3)
+console.log(str5)
+console.log(str2)
+console.log(str6)
+console.log(str4)
+console.log(str1)
